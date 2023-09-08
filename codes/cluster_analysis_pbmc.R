6a2fb6912ccbfd8c9447ecf47c3dce31aeb004d0
debnathk <debnathk1997@gmail.com>
2023-09-08 15:59:12 -0400

    Modify code

--- codes/cluster_analysis_pbmc.R
+++ codes/cluster_analysis_pbmc.R
@@ -7,3 +7,19 @@ pbmc.data <- Read10X(data.dir = "data/filtered_gene_bc_matrices/hg19/")
 # Initialize the Seurat object with the raw (non-normalized data)
 pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
 pbmc
+
+# QC and cell selection
+
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+## Visualize QC metrics as violin plots
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+## Feature scatter Plot
+plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+# Normalizing the data
