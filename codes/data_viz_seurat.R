b12ecf9e9e87cd09e7250a9072c610040f5dd666
Kusal Debnath <debnathk1997@gmail.com>
2023-12-29 23:02:24 -0500

    Modified data_viz_seurat.R

---
+++ codes/data_viz_seurat.R
@@ -0,0 +1,11 @@
+# install.packages("remotes")
+# remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
+# remotes::install_github("satijalab/seurat-data")
+
+# Download data
+SeuratData::InstallData("pbmc3k")
+
+
+
+
+
