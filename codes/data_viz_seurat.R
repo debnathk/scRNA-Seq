4774bbaa96d220c955578fbe79337e93579d538c
Kusal Debnath <debnathk1997@gmail.com>
2023-12-30 21:15:36 -0500

    Modified files

--- codes/data_viz_seurat.R
+++ codes/data_viz_seurat.R
@@ -1,11 +1,22 @@
 # install.packages("remotes")
 # remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE)
 # remotes::install_github("satijalab/seurat-data")
+# SeuratData::InstallData("pbmc3k")
 
-# Download data
-SeuratData::InstallData("pbmc3k")
+# Load libraries
+library(Seurat)
+library(SeuratData)
+library(ggplot2)
+library(patchwork)
 
+pbmc3k.final <- LoadData("pbmc3k", type = "pbmc3k.final")
+pbmc3k.final$groups <- sample(c("group1", "group2"), size = ncol(pbmc3k.final),
+                              replace = TRUE)
+features <- c("LYZ", "CCL5", "IL32", "PTPRCAP", "FCGR3A", "PF4")
+pbmc3k.final
 
-
+# Five visualizations of marker feature expression
+# Ridge plots - to visualize single-cell expression distributions in each cluster
+RidgePlot(pbmc3k.final, features = features, ncol = 2)
 
 
