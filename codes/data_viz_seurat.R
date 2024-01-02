a53f69101c63ba631647b25cb8b753c89125a178
Kusal Debnath <debnathk1997@gmail.com>
2024-01-02 13:02:56 -0500

    Modified file, created plots

--- codes/data_viz_seurat.R
+++ codes/data_viz_seurat.R
@@ -9,6 +9,7 @@ library(SeuratData)
 library(ggplot2)
 library(patchwork)
 
+
 pbmc3k.final <- LoadData("pbmc3k", type = "pbmc3k.final")
 pbmc3k.final$groups <- sample(c("group1", "group2"), size = ncol(pbmc3k.final),
                               replace = TRUE)
@@ -20,11 +21,28 @@ pbmc3k.final
 # Ridge plots - to visualize single-cell expression distributions in each cluster
 RidgePlot(pbmc3k.final, features = features, ncol = 2)
 
+
 # Violin plots - to visualize single-cell expression distributions in each cluster
 VlnPlot(pbmc3k.final, features = features)
 
+
 # Feature plot - to visualize feature expression in low-dimensional space
 FeaturePlot(pbmc3k.final, features = features)
+# Plot a legend to map colors to expression levels
+FeaturePlot(pbmc3k.final, features = "MS4A1")
+# Adjust the contrast in the plot
+FeaturePlot(pbmc3k.final, features = "MS4A1", min.cutoff = 1, max.cutoff = 3)
+
+
+# Dot plot - the size of the dot corresponds to the percentage of cells expressing
+# the feature in each cluster. The color represents the average expression level
+DotPlot(pbmc3k.final, features = features) + RotatedAxis()
+
+
+# Single-cell heatmap of feature expression
+DoHeatmap(subset(pbmc3k.final, downsample = 100), features = features, size = 3)
+
+
 
 
 
