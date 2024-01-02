1c4f62eee6a4206e0d770a3fbce10ac1a3115a98
Kusal Debnath <debnathk1997@gmail.com>
2024-01-02 01:30:18 -0500

    Modified file, added results

--- codes/data_viz_seurat.R
+++ codes/data_viz_seurat.R
@@ -12,11 +12,20 @@ library(patchwork)
 pbmc3k.final <- LoadData("pbmc3k", type = "pbmc3k.final")
 pbmc3k.final$groups <- sample(c("group1", "group2"), size = ncol(pbmc3k.final),
                               replace = TRUE)
-features <- c("LYZ", "CCL5", "IL32", "PTPRCAP", "FCGR3A", "PF4")
+features <- c("CCL5", "IL32", "LYZ", "PTPRCAP", "FCGR3A", "PF4")
 pbmc3k.final
+
 
 # Five visualizations of marker feature expression
 # Ridge plots - to visualize single-cell expression distributions in each cluster
 RidgePlot(pbmc3k.final, features = features, ncol = 2)
+
+# Violin plots - to visualize single-cell expression distributions in each cluster
+VlnPlot(pbmc3k.final, features = features)
+
+# Feature plot - to visualize feature expression in low-dimensional space
+FeaturePlot(pbmc3k.final, features = features)
+
+
 
 
