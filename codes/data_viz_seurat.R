bc2ba54271e78ff43e73224a167f8fed45863cec
Kusal Debnath <debnathk1997@gmail.com>
2024-01-03 08:56:00 -0500

    Modified code, created plots

--- codes/data_viz_seurat.R
+++ codes/data_viz_seurat.R
@@ -13,6 +13,9 @@ library(patchwork)
 pbmc3k.final <- LoadData("pbmc3k", type = "pbmc3k.final")
 pbmc3k.final$groups <- sample(c("group1", "group2"), size = ncol(pbmc3k.final),
                               replace = TRUE)
+# The above code randomly assigns cells to a new meta.data column to use as
+# an example in demonstrating plotting functions that can split or group plots by
+# another valid meta.data variable.
 features <- c("CCL5", "IL32", "LYZ", "PTPRCAP", "FCGR3A", "PF4")
 pbmc3k.final
 
@@ -32,7 +35,14 @@ FeaturePlot(pbmc3k.final, features = features)
 FeaturePlot(pbmc3k.final, features = "MS4A1")
 # Adjust the contrast in the plot
 FeaturePlot(pbmc3k.final, features = "MS4A1", min.cutoff = 1, max.cutoff = 3)
-
+# Calculate feature-specific contrast levels based on quantiles of non-zero expression
+# Particularly useful when plotting multiple markers
+FeaturePlot(pbmc3k.final, features = c("MS4A1", "PTPRCAP"), min.cutoff = "q10",
+            max.cutoff = "q90")
+# Visualize co-expression of two features simultaneously
+FeaturePlot(pbmc3k.final, features = c("MS4A1", "CD79A"), blend = TRUE)
+# Split visualization to view expression by groups (replaces FeatureHeatMap)
+FeaturePlot(pbmc3k.final, features = c("MS4A1", "CD79A"), split.by = "groups")
 
 # Dot plot - the size of the dot corresponds to the percentage of cells expressing
 # the feature in each cluster. The color represents the average expression level
@@ -41,6 +51,7 @@ DotPlot(pbmc3k.final, features = features) + RotatedAxis()
 
 # Single-cell heatmap of feature expression
 DoHeatmap(subset(pbmc3k.final, downsample = 100), features = features, size = 3)
+
 
 
 
