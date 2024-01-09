8e72f145a78ea488a7fcbb626189f0083201f32e
Kusal Debnath <debnathk1997@gmail.com>
2024-01-09 11:31:33 -0500

    Created sctransform.R

---
+++ codes/sctransform.R
@@ -0,0 +1,6 @@
+# sctransform:
+# - A modelling framework for the normalization and variance stabilization of
+# molecular count data from scRNA-Seq experiments, diminishing the need for
+# heuristic steps including pseudocount addition, log-transformation and improves
+# common downstream analytical tasks such as variable gene selection, dimensional
+# reduction, and differential gene expression.
