be2e9bf6538037ed4d94e377064a83aa4525c65f
Kusal Debnath <debnathk1997@gmail.com>
2024-01-09 11:38:48 -0500

    Modified code

--- codes/sctransform.R
+++ codes/sctransform.R
@@ -4,3 +4,14 @@
 # heuristic steps including pseudocount addition, log-transformation and improves
 # common downstream analytical tasks such as variable gene selection, dimensional
 # reduction, and differential gene expression.
+
+# Load libraries
+library(Seurat)
+library(ggplot2)
+library(sctransform)
+
+# Load data and create seurat object
+pbmc_data <- Read10X(data.dir = "data/filtered_gene_bc_matrices/hg19")
+pbmc <- CreateSeuratObject(counts = pbmc_data)
+
+# Apply sctransform normalization
