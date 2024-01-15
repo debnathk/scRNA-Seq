9048d56d5c60a627a0b1705400937d14d117d1ec
Kusal Debnath <debnathk1997@gmail.com>
2024-01-15 14:04:10 -0500

    Created code

--- codes/multimodal.R
+++ codes/multimodal.R
@@ -1,1 +1,20 @@
 # https://satijalab.org/seurat/articles/multimodal_vignette
+
+# Dive into the scientific groove where single-cell genomics becomes the ultimate
+# multitasker! Picture this: cells revealing their secrets in a symphony of data types,
+# dancing together in a sensational performance of multimodal analysis.
+# It's not just science; it's a cellular carnival of discovery! 🧬🎉 #GenomicsParty
+
+# Example: CITE-seq enables simultaneous measurement of transcriptomes and cell-surface proteins
+# from the same cell.
+
+# Cell Hashing is a groundbreaking technique that tackles challenges in single-cell
+# genomics head-on! Imagine using special oligo-tagged antibodies that act like
+# personalized ID tags for cells. These tags, targeting universally expressed surface
+# proteins, give each cell a unique label. Now, here's the magic: you can pool these
+# cells together and, by sequencing the tags alongside their transcriptome, unveil
+# their individual identities. It's like giving each cell a passport for its origin!
+# This not only helps overcome batch effects and detect multiplets but also
+# supercharges commercial droplet-based systems, making experiments more cost-effective.
+# It's genomics with a touch of spy intrigue – Cell Hashing, where cells reveal their
+# secrets with style! 🕵️‍♂️🔍🧬 #CellHashingRevolution
