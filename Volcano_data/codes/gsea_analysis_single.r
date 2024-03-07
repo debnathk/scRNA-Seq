b45bf2ba50b1d4d1bbeae34a209e542c213b088d
Kusal Debnath <debnathk1997@gmail.com>
2024-03-07 03:36:54 -0500

    Code added

--- Volcano_data/codes/gsea_analysis_single.r
+++ Volcano_data/codes/gsea_analysis_single.r
@@ -3,17 +3,18 @@ BiocManager::install("fgsea")
 library(fgsea)
 library(dplyr)
 library(clusterProfiler)
+library(ggplot2)
 library("DOSE")
-setwd("/Users/lodimk2/Documents/schwartz_analysis")
+setwd("C:/Users/debnathk/Desktop/study/scRNA-Seq/Volcano_data/Volcano_sig")
 
 # Read in the rank_table as a csv
-rank_table <- read.csv("Volcano_data/Volcano_sig/Titan_TCPS_sig.csv")
+rank_table <- read.csv("mSLA_PT_downsig.csv")
 
 # Sort the table by adjusted pvalue
 rank_table <- rank_table %>% arrange((padj))
 
 # Use padj cutoff of 0.01, this is customizable based on specific requirements. 
-rank_table <- rank_table %>% filter(padj < 0.01)
+rank_table <- rank_table %>% filter(padj < 0.1)
 
 # convert gene symbols to Entrez ID
 # NOTE: species is rat, so convert from human Db to rat Db. 
@@ -28,11 +29,10 @@ ego <- enrichGO(gene          = eg$ENTREZID,
                 qvalueCutoff  = 0.05,
                 readable      = TRUE)
 
-# Create the figure 
-
-overrepresented_figure <- barplot(ego, showCategory=10, title = "Overrepresented GO BPs in Titan_TCPS")
+# Create the figure
+overrepresented_figure <- barplot(ego, showCategory=10, title = "Overrepresented GO BPs in mSLA_PT")
 
 # Save figure
-ggsave("Titan_TCPS_overrepresented_figure.png", plot = overrepresented_figure, width = 10, height = 6, units = "in", dpi = 300)
+ggsave("test_figure.png", plot = overrepresented_figure, width = 10, height = 6, units = "in", dpi = 300)
 
 
