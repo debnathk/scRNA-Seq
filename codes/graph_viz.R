0202836876ad516efe6d8f506157fdfc7fe7b2e2
Kusal Debnath <debnathk1997@gmail.com>
2024-04-10 23:35:42 -0400

    Add code

--- codes/graph_viz.R
+++ codes/graph_viz.R
@@ -8,7 +8,7 @@ files = list.files(pattern = "\\_final.tsv")
 for (file in files){
   network_graph <- read.csv(file, sep = "\t", header = FALSE)
   colnames(network_graph) <- c("Edge1", "Edge2", "Weight")
-  processed_graph <- network_graph %>% filter(Weight > 0.95)
+  processed_graph <- network_graph %>% filter(Weight > 0.975)
   
   edge_list <- processed_graph %>% select(Edge1, Edge2)
   
@@ -17,12 +17,12 @@ for (file in files){
   
   # Saves the plot
   file_sub = sub(".tsv", "", file)
-  plot_name = paste("D:/scRNA-Seq_data/final_schwartz_networks/", file_sub, "_095.png", sep = "")
+  plot_name = paste("D:/scRNA-Seq_data/final_schwartz_networks/", file_sub, "_0975.png", sep = "")
   png(plot_name, width = 4800, height = 3200)
   # Creates the plot
-  plot(igraph_obj, edge.arrow.size=.5, vertex.color="gold", vertex.size=1,
+  plot(igraph_obj, edge.arrow.size=.5, vertex.color="gold", vertex.size=3,
        vertex.frame.color="black", vertex.label.color="black",
-       vertex.label.cex=0.8, vertex.label.dist=3, edge.curved=0.2)
+       vertex.label.cex=1, vertex.label.dist=3, edge.curved=0.2)
   # Close the device to save the plot
   dev.off()
 
@@ -32,6 +32,6 @@ for (file in files){
   sorted_degree <- degrees %>% arrange(desc(degree))
   degree_file_sub = sub(".tsv", "", file)
   degree_file_name = paste("D:/scRNA-Seq_data/final_schwartz_networks/sorted_degrees_",
-                           file_sub, "_095.csv", sep = "")
+                           file_sub, "_0975.csv", sep = "")
   write.csv(sorted_degree, degree_file_name)
 }
